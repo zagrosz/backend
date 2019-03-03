a134eb681f6a358e81be5c72964bc2bf9c4b5707
pietre <pietre@users.noreply.github.com>
2019-03-03 13:22:47 +0000

    backend-5: Added item get all endpoint (#4)

---
+++ src/main/java/pl/zagrosz/resource/ItemResource.java
@@ -0,0 +1,30 @@
+package pl.zagrosz.resource;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import pl.zagrosz.dao.entity.Item;
+import pl.zagrosz.service.ItemService;
+
+@RestController
+@RequestMapping("/items")
+public class ItemResource {
+
+  private final ItemService itemService;
+
+  @Autowired
+  public ItemResource(ItemService itemService) {
+    this.itemService = itemService;
+  }
+
+  @GetMapping
+  public ResponseEntity getAll() {
+    List<Item> itemList = itemService.getAll();
+
+    return ResponseEntity.status(HttpStatus.OK).body(itemList);
+  }
+}
---
+++ src/main/java/pl/zagrosz/service/ItemService.java
@@ -0,0 +1,23 @@
+package pl.zagrosz.service;
+
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import pl.zagrosz.dao.entity.Item;
+import pl.zagrosz.dao.repository.ItemRepository;
+
+@Service
+public class ItemService {
+
+  private final ItemRepository itemRepository;
+
+  @Autowired
+  public ItemService(ItemRepository itemRepository) {
+    this.itemRepository = itemRepository;
+  }
+
+  public List<Item> getAll() {
+    return this.itemRepository.findAll();
+  }
+
+}
